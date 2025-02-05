1a9fd2f65bc0e9a15bb409d4db9bc0e137be2dc0
Vince Sevilla <133684091+VinceSevilla@users.noreply.github.com>
2025-02-05 01:42:22 +0000

    init

---
+++ Activity1.sh
@@ -0,0 +1,14 @@
+USER_NAME=""
+AGE=0
+USER_PROGRAM=""
+
+echo "Enter your name: "
+read USER_NAME
+
+echo "Enter you age: "
+read AGE
+
+echo "Enter your program: "
+read USER_PROGRAM
+
+echo "Hello $USER_NAME this is your $AGE and your program is $USER_PROGRAM"
---
+++ sample.sh
@@ -0,0 +1,15 @@
+USER_NAME="Vince"
+AGE=20
+USER_PROGRAM="BSCS"
+
+echo "Enter your name:"
+read USER_NAME
+
+echo "Enter you age:"
+read AGE
+
+echo "Enter your program:"
+read USER_PROGRAM
+
+echo "Hello $USER_NAME this is your $AGE and your program is $USER_PROGRAM"
+
